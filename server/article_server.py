54cff9f607598b9eed2ad5f048859b7df3067ad4
Z2Y <zzy277507631@gmail.com>
2015-08-20 18:49:13 +0800

    update blog

--- server/article_server.py
+++ server/article_server.py
@@ -42,48 +42,62 @@ def post(form, user, is_draft):
         has.md_content = content_list[1]
     oj = form.problem_oj_name.data
     pid = form.problem_pid.data
+    has.is_top = form.is_top.data
     has.is_draft = is_draft
     has.problem_oj_name = oj
     has.problem_pid = pid
     has.tags = tags
     has.save()
 
-def get_list(offset=0, limit=20,user=None):
+
+def filter_query(query_type=None, keyword=''):
+    if query_type == 'title' and keyword != '':
+        query = SolutionArticle.query.filter(SolutionArticle.title.like('%' + keyword + '%'))
+    elif query_type == 'tag' and keyword != '':
+        tag_row = Tag.query.filter(Tag.name==keyword).first()
+        query = tag_row.solutions if tag_row else None
+    else:
+        query = SolutionArticle.query
+    return query
+
+def get_list(offset=0, limit=20, user=None, query_type=None, keyword=''):
     if not user:
-        return SolutionArticle.query.filter(SolutionArticle.is_draft==0).\
-            order_by(SolutionArticle.last_update_time.desc()).\
-            offset(offset).limit(limit).all()
+        query = filter_query(query_type, keyword)
+        return query.filter(SolutionArticle.is_draft==0).\
+            order_by(SolutionArticle.is_top.desc(), SolutionArticle.last_update_time.desc()).\
+            offset(offset).limit(limit).all() if query else []
     elif user.is_admin:
         return SolutionArticle.query.\
-            order_by(SolutionArticle.last_update_time.desc()).\
+            order_by(SolutionArticle.is_top.desc(), SolutionArticle.last_update_time.desc()).\
             offset(offset).limit(limit).all()
     elif user.is_coach:
         return SolutionArticle.query.join(SolutionArticle.user)\
             .filter(User.school==user.school, User.rights < 4).\
-            order_by(SolutionArticle.last_update_time.desc()).\
+            order_by(SolutionArticle.is_top.desc(), SolutionArticle.last_update_time.desc()).\
             offset(offset).limit(limit).all()
     else:
         return SolutionArticle.query.filter(SolutionArticle.user==user).\
-            order_by(SolutionArticle.last_update_time.desc()).\
+            order_by(SolutionArticle.is_top.desc(), SolutionArticle.last_update_time.desc()).\
             offset(offset).limit(limit).all()
 
-def get_count(user=None):
+def get_count(user=None, query_type=None, keyword=''):
     if not user:
-        return SolutionArticle.query.filter(SolutionArticle.is_draft==0).\
-            order_by(SolutionArticle.last_update_time.desc()).\
-            count()
+        query = filter_query(query_type, keyword)
+        return query.filter(SolutionArticle.is_draft==0).\
+            order_by(SolutionArticle.is_top.desc(), SolutionArticle.last_update_time.desc()).\
+            count() if query else 0
     elif user.is_admin:
         return SolutionArticle.query.\
-            order_by(SolutionArticle.last_update_time.desc()).\
+            order_by(SolutionArticle.is_top.desc(), SolutionArticle.last_update_time.desc()).\
             count()
     elif user.is_coach:
         return SolutionArticle.query.join(SolutionArticle.user)\
             .filter(User.school==user.school, User.rights < 4).\
-            order_by(SolutionArticle.last_update_time.desc()).\
+            order_by(SolutionArticle.is_top.desc(), SolutionArticle.last_update_time.desc()).\
             count()
     else:
         return SolutionArticle.query.filter(SolutionArticle.user==user).\
-            order_by(SolutionArticle.last_update_time.desc()).\
+            order_by(SolutionArticle.is_top.desc(), SolutionArticle.last_update_time.desc()).\
             count()
 
 def get_recent(limit=5):
@@ -125,3 +139,25 @@ def get_archive_by_tag(tag):
             archives[year] = []
         archives[year].append(article)
     return archives
+
+def get_all_tags():
+    tags_row = Tag.query.filter(Tag.solutions!=None).all()
+    tags = []
+    for tag in tags_row:
+        if tag.solutions.filter(SolutionArticle.is_draft==0).count():
+            tags.append(tag)
+    return tags
+
+def related_submits(article, offset=0, limit=10):
+    if article.problem_oj_name == '' or article.problem_pid == '':
+        return []
+    query = Submit.query.filter(Submit.oj_name==article.problem_oj_name,Submit.pro_id==article.problem_pid)\
+        #filter(or_(Submit.result == 'OK', Submit.result == 'Accepted')).all()
+    return query.offset(offset).limit(limit).all()
+
+def related_submits_count(article):
+    if article.problem_oj_name == '' or article.problem_pid == '':
+        return 0
+    query = Submit.query.filter(Submit.oj_name==article.problem_oj_name,Submit.pro_id==article.problem_pid)\
+        #filter(or_(Submit.result == 'OK', Submit.result == 'Accepted')).all()
+    return query.count()
